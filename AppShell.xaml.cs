a739945c7b74989f1501731047a0008209dd9337
LawDerrthy <145079518+LawDerrthy@users.noreply.github.com>
2025-06-13 20:44:42 -0600

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using EcoAppODS.Pages;
+
+namespace EcoAppODS;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // Si más adelante usara rutas personalizadas agregar:
+        // Routing.RegisterRoute(nameof(ReportePage), typ eof(ReportePage));
+        // Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
+    }
+}
